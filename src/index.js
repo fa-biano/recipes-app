67d941569ac131253de465fd96be1a418192c755
Fa-biano <fbiano.souza@gmail.com>
2022-11-19 20:54:20 -0300

    config github pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import Provider from './contextApi/Provider';
 
@@ -9,8 +9,8 @@ ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <Provider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>,
   );
